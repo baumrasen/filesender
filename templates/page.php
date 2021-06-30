9000bc21af09b62ab0318ac4842d39933341fb32
Andreas <baumrasen@gmail.com>
2021-06-30 12:29:51 +0200

    added sitename to page-header

--- templates/page.php
+++ templates/page.php
@@ -1,10 +1,23 @@
+<div id="site-name" class="text-right">
+<div class="container">
+        <div class="col_content">
+                ibb Konstruktionsdienstleistungs GmbH
+        </div>
+</div>
+</div>
+
+
+<div id="page-header-wrapper">
+</div>
+
 <div id="page" class="<?php echo GUI::currentPage() ?>_page">
-    
+<div class="container">
     <noscript>
         <div class="error message">
             {tr:noscript}
         </div>
     </noscript>
-    
+
     <?php Template::display(GUI::currentPage().'_page', $vars) ?>
+</div>
 </div>
